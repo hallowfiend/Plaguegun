c0139422d5b1174202157e46d3d783d500e209a8
Nikolai Jakobslock <hallowfiend@protonmail.com>
2025-08-03 16:40:50 +0200

    Dodaj pliki projektów.

---
+++ Source/Properties/Plaguebulletproper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace TST_PlagueGun
+{
+    public class Projectile_PlagueBullet : Bullet
+    {
+        public ModExtension_PlagueBullet Props => def.GetModExtension<ModExtension_PlagueBullet>();
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            base.Impact(hitThing, blockedByShield);
+            if (Props != null && hitThing != null && hitThing is Pawn hitPawn)
+            {
+                float rand = Rand.Value;
+                if (rand <= Props.addHediffChance)
+                {
+                    Messages.Message("TST_PlagueBullet_SuccessMessage".Translate(this.launcher.Label, hitPawn.Label), MessageTypeDefOf.NeutralEvent);
+                    Hediff plagueOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.hediffToAdd);
+                    float randomSeverity = Rand.Range(0.15f, 0.30f);
+                    if (plagueOnPawn != null)
+                        {
+	                        plagueOnPawn.Severity += randomSeverity;
+                        }
+                    else
+                        {
+                            Hediff hediff = HediffMaker.MakeHediff(Props.hediffToAdd, hitPawn);
+                            hediff.Severity = randomSeverity;
+	                        hitPawn.health.AddHediff(hediff);
+                        }
+                }
+                else
+                    {
+                        MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "TST_PlagueBullet_FailureMote".Translate(Props.addHediffChance), 12f);
+                    }
+            }
+        }
+    }
+}
